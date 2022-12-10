a5547101c0976bc36794a7a91c3df2489c0c52d0
Animesh Kahara <animeshcyberphysco@gmail.com>
2022-12-11 02:50:06 +0530

    Added read_file function.

--- rat.c
+++ rat.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
 
 int main(){
-	printf("Hello Rat!\n");
+	read_file("msg.txt");
 	return 0;
+}
+
+void read_file(char *filename){
+ char ch;
+ FILE *fp = fopen(filename,"r");
+ 
+ // Handle file reading error
+ if(fp == NULL){
+	 printf("\nUnable to open the file!\n");
+	 return;
+ }
+
+ //loop to read file char-by-char
+ ch = fgetc(fp);
+ while(!feof(fp)){
+	printf("%c",ch);
+	ch = fgetc(fp);
+ }
+ fclose(fp);
 }
